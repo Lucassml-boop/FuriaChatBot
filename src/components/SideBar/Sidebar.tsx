bcf1cbb67bee24331e7ee4e954f3c213f7233cf6
Lucassml-boop <lucas.samuel23borges@gmail.com>
2025-04-25 17:42:03 -0300

    ajuste sidebar

--- src/components/SideBar/Sidebar.tsx
+++ src/components/SideBar/Sidebar.tsx
@@ -1,7 +1,10 @@
 import { Home, MessageCircle, Settings } from 'lucide-react';
 import '../../styles/Sidebar.scss';
+import { useTheme } from '../context/ThemeContext';
 
 const Sidebar = () => {
+  const { setTheme } = useTheme();
+
   return (
     <aside className="sidebar">
       <nav className="sidebar_nav">
