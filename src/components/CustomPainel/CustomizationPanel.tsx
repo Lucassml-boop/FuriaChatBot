ef4c0e9bd8958d6b4a64e3c30f5ce703eb4e2914
Lucassml-boop <lucas.samuel23borges@gmail.com>
2025-04-25 17:17:43 -0300

    Atualizando as funções de troca de cores

--- src/components/CustomPainel/CustomizationPanel.tsx
+++ src/components/CustomPainel/CustomizationPanel.tsx
@@ -1,36 +1,59 @@
+import { useState } from 'react';
 import { useTheme } from '../context/ThemeContext';
 import '../../styles/CustomizationPanel.scss';
+import { Sun, Moon } from 'lucide-react';
 
 const CustomizationPanel = () => {
   const { setTheme, setAccentColor, setBotMessageColor } = useTheme();
+  const [activeTheme, setActiveTheme] = useState<'light' | 'dark'>('light');
+
+  const handleThemeChange = (theme: 'light' | 'dark') => {
+    setTheme(theme);
+    setActiveTheme(theme);
+  };
 
   return (
     <div className="customization">
       <div className="customization-itens">
-            <h3>Change the colors to customize your bot</h3>
+        <h3>Change the colors to customize your bot</h3>
 
         <div className="theme-buttons">
-          <button onClick={() => setTheme('light')}>Light</button>
-         <button onClick={() => setTheme('dark')}>Dark</button>
+          <button
+            onClick={() => handleThemeChange('light')}
+            className={activeTheme === 'light' ? 'active' : ''}
+            title="Tema Claro"
+          >
+            <Sun size={24} />
+          </button>
+          <button
+            onClick={() => handleThemeChange('dark')}
+            className={activeTheme === 'dark' ? 'active' : ''}
+            title="Tema Escuro"
+          >
+            <Moon size={24} />
+          </button>
         </div>
 
         <h4>Accent Color</h4>
         <div className="color-options">
-          {['#e63946', '#f1c40f', '#2ecc71', '#3498db', '#8a2be2', '#ff6f61', '#00cec9'].map(color => (
+          {['#e63946', '#f1c40f', '#2ecc71', '#3498db', '#8a2be2', '#ff6f61', '#00cec9', '#fff'].map((color) => (
             <button
-             key={color}
-             style={{ backgroundColor: color }}
+              key={color}
+              style={{ backgroundColor: color }}
               onClick={() => setAccentColor(color)}
             />
           ))}
         </div>
 
         <h4>Bot Message</h4>
-        <div className="color-options-bot">
-          <button  onClick={() => setBotMessageColor('#e63946')}>Red</button>
-         <button  onClick={() => setBotMessageColor('#3498db')}>
-           Blue
-          </button>
+        <div className="color-options">
+          {['#e63946', '#f1c40f', '#2ecc71', '#3498db', '#8a2be2', '#ff6f61', '#00cec9', '#eeeeee'].map((color) => (
+            <button
+              key={color}
+              style={{ backgroundColor: color }}
+              onClick={() => setBotMessageColor(color)}
+            />
+          ))}
         </div>
       </div>
     </div>
