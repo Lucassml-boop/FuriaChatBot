b03b70601f400d6f2938896194dd8f180e92816a
Lucassml-boop <lucas.samuel23borges@gmail.com>
2025-05-15 09:06:28 -0300

    Melhorando a responsividade

--- src/components/CustomPainel/CustomizationPanel.tsx
+++ src/components/CustomPainel/CustomizationPanel.tsx
@@ -2,19 +2,34 @@ import { useState } from 'react';
 import { useTheme } from '../context/ThemeContext';
 import '../../styles/CustomizationPanel.scss';
 import { Sun, Moon } from 'lucide-react';
+import { Settings } from 'lucide-react';
 
 const CustomizationPanel = () => {
   const { setTheme, setAccentColor, setBotMessageColor } = useTheme();
   const [activeTheme, setActiveTheme] = useState<'light' | 'dark'>('light');
+  const [isOpen, setIsOpen] = useState(false);
 
   const handleThemeChange = (theme: 'light' | 'dark') => {
     setTheme(theme);
     setActiveTheme(theme);
   };
 
+  const togglePanel = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <div className="customization">
-      <div className="customization-itens">
+    <div className={`customization ${isOpen ? 'open' : ''}`}>
+      
+      <div
+        className="customization-button"
+        onClick={togglePanel}
+        title="Abrir painel de personalização"
+      >
+        <Settings size={19} />
+      </div>
+
+      <div className={`customization-itens ${isOpen || window.innerWidth > 768 ? 'open' : ''}`}>
         <h3>Change the colors to customize your bot</h3>
 
         <div className="theme-buttons">
